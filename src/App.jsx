26d2fcf06ecca42c0634a47fd6e49b72ff5953c6
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-11 17:43:54 +0200

    Add initial fetch logic to retrieve actor data on app load and log results

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-
-import './App.css'
+// App.jsx
+import { useEffect } from 'react';
 
 function App() {
- 
+  // useEffect viene eseguito una sola volta al caricamento del componente
+  useEffect(() => {
+    console.log("App caricata!");
+
+    // Eseguiamo il fetch verso l'API degli attori
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log("Risposta ricevuta! Ora la trasformo in JSON...");
+        return res.json(); // trasformiamo la risposta in JSON
+      })
+      .then((data) => {
+        console.log("Lista attori ricevuta! 🎬");
+        console.log(data); // stampiamo i dati in console
+      })
+      .catch((error) => {
+        console.log("Errore nel recupero dati ", error);
+      });
+  }, []); // [] significa che useEffect si esegue solo una volta, all'inizio
 
   return (
-    <>
-      <h1>Cast Fetching</h1>
-    </>
-  )
+    <div>
+      <h1>Lista Attori</h1>
+      <p>Apri la console per vedere i dati!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
